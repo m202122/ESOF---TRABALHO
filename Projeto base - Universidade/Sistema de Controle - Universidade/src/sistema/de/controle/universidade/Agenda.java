5467d23fd155283e33edd722e18daa4f61b30079
Marco Aurélio Bosque <MarcoAurelio@Outlook.com>
2013-09-17 13:38:20 -0700

    =)

--- Projeto base - Universidade/Sistema de Controle - Universidade/src/sistema/de/controle/universidade/Agenda.java
+++ Projeto base - Universidade/Sistema de Controle - Universidade/src/sistema/de/controle/universidade/Agenda.java
@@ -95,6 +95,7 @@ public class Agenda extends javax.swing.JFrame {
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
+        new InserirContatos();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     
