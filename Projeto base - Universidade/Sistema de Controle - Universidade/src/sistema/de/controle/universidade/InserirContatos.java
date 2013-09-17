087e31d2ca6fd2862e9176920a444e121b4c7abb
Marco Aurélio Bosque <MarcoAurelio@Outlook.com>
2013-09-17 14:06:02 -0700

    =)

--- Projeto base - Universidade/Sistema de Controle - Universidade/src/sistema/de/controle/universidade/InserirContatos.java
+++ Projeto base - Universidade/Sistema de Controle - Universidade/src/sistema/de/controle/universidade/InserirContatos.java
@@ -8,7 +8,6 @@ public class InserirContatos extends javax.swing.JFrame {
     public InserirContatos() {
         initComponents();
         setVisible(true);
-        insere();
         setTitle("Inserir novo contato");
     }
     @SuppressWarnings("unchecked")
@@ -23,20 +22,22 @@ public class InserirContatos extends javax.swing.JFrame {
             String telefone = jTextField2.getText().toString();
             String email = jTextField3.getText().toString();
             String extra = jTextArea1.getText().toString();
-            if(nome.equals("")||telefone.equals("")||email.equals(""))
+            if(nome.equals(""))
             {
-                JOptionPane.showMessageDialog( this, "Campo nome não preenchido" );
+                JOptionPane.showMessageDialog( this, "Campo nome em branco" );
             }
-            String query = "INSERT INTO universidade.contatos(id_estudante,nome_contato,telefone,email,extra) VALUES ('"+ControleUniversidade.a+"','"+nome+"','"+telefone+"','"+email+"','"+extra+"')";        
-            int r;
-            r = cd.updatequery(query);
-            if(r == 1)
-            {
-                JOptionPane.showMessageDialog( this, "Novo contato inserido!" );
-            }
-            else
-            {
-                JOptionPane.showMessageDialog( this, "Novo contato não inserido!" );
+
+            else{
+                String query = "INSERT INTO universidade.contatos(id_estudante,nome_contato,telefone,email,extra) VALUES ('"+ControleUniversidade.a+"','"+nome+"','"+telefone+"','"+email+"','"+extra+"')";        
+                int r;
+                r = cd.updatequery(query);
+                if(r == 1)
+                {
+                     JOptionPane.showMessageDialog( this, "Novo contato inserido!" );
+                }
+                else {
+                     JOptionPane.showMessageDialog( this, "Novo contato não inserido!" );
+                }
             }
         }
         catch ( Exception sqlex ) {
@@ -60,6 +61,7 @@ public class InserirContatos extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -79,6 +81,18 @@ public class InserirContatos extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTextArea1);
 
         jButton1.setText("ADICIONAR NOVO CONTATO");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("VOLTAR AO MENU ANTERIOR");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -105,10 +119,11 @@ public class InserirContatos extends javax.swing.JFrame {
                                 .addComponent(jLabel4)
                                 .addGap(0, 138, Short.MAX_VALUE))))
                     .addComponent(jScrollPane1)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(TextBaseDadosProfessor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -135,15 +150,28 @@ public class InserirContatos extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton1)
-                .addGap(0, 15, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addGap(0, 11, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        insere();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private java.awt.Label TextBaseDadosProfessor;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
