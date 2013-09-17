ce37e98064d91bd5ae238adcf74d8062887c5bec
Marco Aurélio Bosque <MarcoAurelio@Outlook.com>
2013-09-17 20:08:54 -0300

    sql
    sql

---
+++ Projeto base - Universidade/Sistema de Controle - Universidade/src/sistema/de/controle/universidade/ExcluirContatos.java
@@ -0,0 +1,239 @@
+package sistema.de.controle.universidade;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class ExcluirContatos extends javax.swing.JFrame {
+    
+    javax.swing.DefaultListModel modelolistacontatos = new javax.swing.DefaultListModel();
+    
+    public ExcluirContatos() throws SQLException {
+        initComponents();
+        setVisible(true);
+        setTitle("Excluir contato");
+        getContatos();
+    }
+
+    @SuppressWarnings("unchecked")
+    
+    private void getContatos() throws SQLException
+    {
+        ResultSet rs = null;
+        ConectarAOBanco cd = new ConectarAOBanco();
+        cd.ConectarBanco();
+        String query = "SELECT c.nome_contato FROM universidade.contatos c WHERE c.id_estudante='"+ControleUniversidade.a+"' ORDER BY c.nome_contato";
+        rs = cd.query(query);
+        int cont = exibirLista(rs);
+        if(cont == 1)
+            jList1.setModel(modelolistacontatos);
+    }
+    
+    public int exibirLista(ResultSet rs) throws SQLException {
+        // position to first record
+        boolean moreRecords = rs.next();
+        // If there are no records, display a message
+        if (!moreRecords) {
+            return 0;
+        }
+        modelolistacontatos.addElement(rs.getString(1));
+        while (rs.next()) {
+            modelolistacontatos.addElement(rs.getString(1));
+        }
+        return 1;
+    }
+    
+    private String getInfo( ResultSet rs ) throws SQLException {
+      // position to first record
+      boolean moreRecords = rs.next();  
+      // If there are no records, display a message
+      if ( ! moreRecords ) { 
+         return null;
+      }
+      Vector columnHeads = new Vector();
+      Vector rows = new Vector();
+      try {
+         // get column heads
+         ResultSetMetaData rsmd = rs.getMetaData();
+         for ( int i = 1; i <= rsmd.getColumnCount(); ++i ) 
+            columnHeads.addElement( rsmd.getColumnName( i ) );
+         // get row data
+         do {
+            rows.addElement( getNextRow( rs, rsmd ) ); 
+         } while ( rs.next() );
+          String info;
+          String aux = rows.get(0).toString();
+          info = aux.substring(1,aux.length()-1);
+          return info;
+      }
+      catch ( SQLException sqlex ) {
+         sqlex.printStackTrace();
+	 return null;
+      }
+   }
+    
+    private Vector getNextRow( ResultSet rs, ResultSetMetaData rsmd ) throws SQLException {
+        Vector currentRow = new Vector();
+        for ( int i = 1; i <= rsmd.getColumnCount(); ++i )
+            switch( rsmd.getColumnType( i ) ) {
+                case Types.VARCHAR:
+                    currentRow.addElement( rs.getString( i ) );
+                    break;
+                case Types.INTEGER:
+                    currentRow.addElement( 
+                    new Long( rs.getLong( i ) ) );
+                    break;
+                case Types.DATE:
+                    currentRow.addElement(rs.getDate( i ));
+                    break;
+                case Types.FLOAT:
+                    currentRow.addElement(rs.getFloat(i));
+                    break;
+                case Types.CHAR:
+                    currentRow.addElement(rs.getString(i));
+                    break;
+                case Types.REAL:
+                    currentRow.addElement(rs.getFloat(i));
+                    break;  
+                default: 
+                    System.out.println( "Type was: " + rsmd.getColumnTypeName( i ) );
+         }
+         return currentRow;
+    }
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        TextBaseDadosProfessor = new java.awt.Label();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        TextBaseDadosProfessor.setFont(new java.awt.Font("Corbel", 1, 18)); // NOI18N
+        TextBaseDadosProfessor.setText("Excluir contato");
+
+        jLabel1.setText("Nomes dos contatos");
+
+        jList1.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jList1);
+
+        jButton1.setText("EXCLUIR CONTATO");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("VOLTAR AO MENU ANTERIOR");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jSeparator1)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(TextBaseDadosProfessor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(TextBaseDadosProfessor, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 2, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addGap(0, 15, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    
+    
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        String nome;
+        ConectarAOBanco cd = new ConectarAOBanco();
+        cd.ConectarBanco();
+        ResultSet rs = null;
+        try
+        {
+            if(jList1.isSelectionEmpty()) {
+                JOptionPane.showMessageDialog( this, "Selecione o contato" );
+                return;
+            }
+            else{
+                nome = jList1.getSelectedValue().toString();
+                System.out.println(nome);
+                String id = "SELECT c.id_contato FROM universidade.contatos c WHERE c.nome_contato='"+nome+"' AND c.id_estudante='"+ControleUniversidade.a+"'";
+                rs = cd.query(id);
+                String id_int = getInfo(rs);
+                String query = "DELETE FROM universidade.contatos c where c.id_contato='"+id_int+"'";
+                int r;
+                r = cd.updatequery(query);
+                if(r == 1)
+                {
+                    modelolistacontatos.removeElement(nome);
+                    JOptionPane.showMessageDialog( this, "Contato foi excluído." );
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog( this, "Contato não foi excluído." );
+                }
+            }
+            
+        } 
+        catch (Exception ex) {
+                ex.printStackTrace();
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private java.awt.Label TextBaseDadosProfessor;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JList jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ Projeto base - Universidade/Sistema de Controle - Universidade/src/sistema/de/controle/universidade/InserirContatos.java
@@ -0,0 +1,186 @@
+package sistema.de.controle.universidade;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class InserirContatos extends javax.swing.JFrame {
+    public InserirContatos() {
+        initComponents();
+        setVisible(true);
+        setTitle("Inserir novo contato");
+    }
+    @SuppressWarnings("unchecked")
+    
+    private void insere()
+    {
+        ResultSet rs = null;
+        ConectarAOBanco cd = new ConectarAOBanco();
+        try {
+            cd.ConectarBanco();
+            String nome = jTextField1.getText().toString();
+            String telefone = jTextField2.getText().toString();
+            String email = jTextField3.getText().toString();
+            String extra = jTextArea1.getText().toString();
+            if(nome.equals(""))
+            {
+                JOptionPane.showMessageDialog( this, "Campo nome em branco" );
+            }
+
+            else{
+                String query = "INSERT INTO universidade.contatos(id_estudante,nome_contato,telefone,email,extra) VALUES ('"+ControleUniversidade.a+"','"+nome+"','"+telefone+"','"+email+"','"+extra+"')";        
+                int r;
+                r = cd.updatequery(query);
+                if(r == 1)
+                {
+                     JOptionPane.showMessageDialog( this, "Novo contato inserido!" );
+                }
+                else {
+                     JOptionPane.showMessageDialog( this, "Novo contato não inserido!" );
+                }
+            }
+        }
+        catch ( Exception sqlex ) {
+            //sqlex.printStackTrace();
+            JOptionPane.showMessageDialog( this, "Não é possível adicionar novo contato" );
+        }
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        TextBaseDadosProfessor = new java.awt.Label();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        TextBaseDadosProfessor.setFont(new java.awt.Font("Corbel", 1, 18)); // NOI18N
+        TextBaseDadosProfessor.setText("Inserir novo contato");
+
+        jLabel1.setText("Nome:");
+
+        jLabel2.setText("Telefone (Ex: 34 8888 8888):");
+
+        jLabel3.setText("Email (Ex: email@email.com ):");
+
+        jLabel4.setText("Informações extras");
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jButton1.setText("ADICIONAR NOVO CONTATO");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("VOLTAR AO MENU ANTERIOR");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jSeparator1)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField3)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addGap(0, 138, Short.MAX_VALUE))))
+                    .addComponent(jScrollPane1)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(TextBaseDadosProfessor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(TextBaseDadosProfessor, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 2, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addGap(0, 11, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        insere();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private java.awt.Label TextBaseDadosProfessor;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    // End of variables declaration//GEN-END:variables
+}
