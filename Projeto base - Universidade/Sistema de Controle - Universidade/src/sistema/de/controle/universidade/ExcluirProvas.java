2f363c907363b72ed17f99f1ff3f60fd75879691
Marco Aurélio Bosque <MarcoAurelio@Outlook.com>
2013-09-17 18:07:29 -0700

    =)

--- Projeto base - Universidade/Sistema de Controle - Universidade/src/sistema/de/controle/universidade/ExcluirProvas.java
+++ Projeto base - Universidade/Sistema de Controle - Universidade/src/sistema/de/controle/universidade/ExcluirProvas.java
@@ -1,10 +1,106 @@
 package sistema.de.controle.universidade;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Vector;
+import javax.swing.JOptionPane;
+
 public class ExcluirProvas extends javax.swing.JFrame {
-    public ExcluirProvas() {
+    
+    javax.swing.DefaultListModel modelolistaprovas = new javax.swing.DefaultListModel();
+    
+    public ExcluirProvas() throws SQLException{
         setTitle("Excluir provas marcadas");
         initComponents();
+        setVisible(true);
+        getProvas();
     }
     @SuppressWarnings("unchecked")
+    private void getProvas() throws SQLException
+    {
+        ResultSet rs = null;
+        ConectarAOBanco cd = new ConectarAOBanco();
+        cd.ConectarBanco();
+        String query = "SELECT p.disciplina FROM universidade.provas p WHERE p.id_aluno='"+ControleUniversidade.a+"' ORDER BY p.disciplina";
+        rs = cd.query(query);
+        int cont = exibirLista(rs);
+        if(cont == 1)
+            jList1.setModel(modelolistaprovas);
+    }
+    
+    public int exibirLista(ResultSet rs) throws SQLException {
+        // position to first record
+        boolean moreRecords = rs.next();
+        // If there are no records, display a message
+        if (!moreRecords) {
+            return 0;
+        }
+        modelolistaprovas.addElement(rs.getString(1));
+        while (rs.next()) {
+            modelolistaprovas.addElement(rs.getString(1));
+        }
+        return 1;
+    }
+    
+    private String getInfo( ResultSet rs ) throws SQLException {
+      // position to first record
+      boolean moreRecords = rs.next();  
+      // If there are no records, display a message
+      if ( ! moreRecords ) { 
+         return null;
+      }
+      Vector columnHeads = new Vector();
+      Vector rows = new Vector();
+      try {
+         // get column heads
+         ResultSetMetaData rsmd = rs.getMetaData();
+         for ( int i = 1; i <= rsmd.getColumnCount(); ++i ) 
+            columnHeads.addElement( rsmd.getColumnName( i ) );
+         // get row data
+         do {
+            rows.addElement( getNextRow( rs, rsmd ) ); 
+         } while ( rs.next() );
+          String info;
+          String aux = rows.get(0).toString();
+          info = aux.substring(1,aux.length()-1);
+          return info;
+      }
+      catch ( SQLException sqlex ) {
+         sqlex.printStackTrace();
+	 return null;
+      }
+   }
+    
+    private Vector getNextRow( ResultSet rs, ResultSetMetaData rsmd ) throws SQLException {
+        Vector currentRow = new Vector();
+        for ( int i = 1; i <= rsmd.getColumnCount(); ++i )
+            switch( rsmd.getColumnType( i ) ) {
+                case Types.VARCHAR:
+                    currentRow.addElement( rs.getString( i ) );
+                    break;
+                case Types.INTEGER:
+                    currentRow.addElement( 
+                    new Long( rs.getLong( i ) ) );
+                    break;
+                case Types.DATE:
+                    currentRow.addElement(rs.getDate( i ));
+                    break;
+                case Types.FLOAT:
+                    currentRow.addElement(rs.getFloat(i));
+                    break;
+                case Types.CHAR:
+                    currentRow.addElement(rs.getString(i));
+                    break;
+                case Types.REAL:
+                    currentRow.addElement(rs.getFloat(i));
+                    break;  
+                default: 
+                    System.out.println( "Type was: " + rsmd.getColumnTypeName( i ) );
+         }
+         return currentRow;
+    }
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -26,6 +122,11 @@ public class ExcluirProvas extends javax.swing.JFrame {
         jScrollPane2.setViewportView(jList1);
 
         jButton1.setText("Excluir prova");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("VOLTAR AO MENU ANTERIOR");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -76,6 +177,44 @@ public class ExcluirProvas extends javax.swing.JFrame {
         // TODO add your handling code here:
         setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        String nome;
+        ConectarAOBanco cd = new ConectarAOBanco();
+        cd.ConectarBanco();
+        ResultSet rs = null;
+        try
+        {
+            if(jList1.isSelectionEmpty()) {
+                JOptionPane.showMessageDialog( this, "Selecione a disciplina" );
+                return;
+            }
+            else{
+                nome = jList1.getSelectedValue().toString();
+                System.out.println(nome);
+                String id = "SELECT p.id_prova FROM universidade.provas p WHERE p.disciplina='"+nome+"' AND p.id_aluno='"+ControleUniversidade.a+"'";
+                rs = cd.query(id);
+                String id_int = getInfo(rs);
+                String query = "DELETE FROM universidade.provas p where p.id_prova='"+id_int+"'";
+                int r;
+                r = cd.updatequery(query);
+                if(r == 1)
+                {
+                    modelolistaprovas.removeElement(nome);
+                    JOptionPane.showMessageDialog( this, "Data da prova excluída." );
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog( this, "Data da prova excluída." );
+                }
+            }
+            
+        } 
+        catch (Exception ex) {
+                ex.printStackTrace();
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
    
     // Variables declaration - do not modify//GEN-BEGIN:variables
